55411e860536cfb1278d354d2c8135d3ffe48e50
jinho222 <jinho020@yonsei.ac.kr>
2023-05-29 16:18:48 +0900

    style setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Noto_Sans_KR } from "@next/font/google";
+
+const notoSansKR = Noto_Sans_KR({
+  weight: ["300", "400", "700", "900"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <main className={notoSansKR.className}>
+        <Component {...pageProps} />
+      </main>
+    </ChakraProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
-import {FC} from "react";
+import { FC } from "react";
+import { Box, Text } from "@chakra-ui/react";
 
 const Landing_Page: FC = () => {
-  return <div>landing</div>
-}
+  return (
+    <div>
+      <Box>
+        <Text>안녕 세계!</Text>
+      </Box>
+    </div>
+  );
+};
 
 export default Landing_Page;
